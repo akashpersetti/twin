302ba21a3efc6baa0ead9d31cda5ded096c059a7
akashpersetti <akash.persetti@gmail.com>
2026-08-03 14:10:16 -0400

    chore: fix terraform fmt alignment in lambda_ssm policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -169,8 +169,8 @@ resource "aws_iam_role_policy" "lambda_ssm" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Effect   = "Allow"
-      Action   = ["ssm:GetParameter"]
+      Effect = "Allow"
+      Action = ["ssm:GetParameter"]
       Resource = [
         "arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter/twin/dev/blog-admin-token",
         "arn:aws:ssm:*:${data.aws_caller_identity.current.account_id}:parameter/twin/dev/github-pat",
